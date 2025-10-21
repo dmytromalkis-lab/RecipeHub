7b14786dc7b52a0544d69bbda1ff7c94bed25f9b
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2025-10-21 20:18:35 +0300

    add login for admin and midlleware for admins routes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import generateToken from "../utils/generateToken.js";
 
@@ -10,8 +11,10 @@ export const register = async (req, res) => {
       return res.status(422).json({ message: "Filds are required" });
     }
 
+    const isAdmin = email == process.env.ADMIN_EMAIL;
     const existUser = await User.findOne({ where: { email } });
-    if (existUser) {
+
+    if (existUser || isAdmin) {
       return res.status(409).json({ message: "User already exists" });
     }
 
@@ -53,6 +56,14 @@ export const login = async (req, res) => {
 
     if (!email || !password) {
       return res.status(422).json({ message: "Filds are required" });
+    }
+
+    if(email == process.env.ADMIN_EMAIL) {
+      if(password != process.env.ADMIN_PASSWORD) {
+        return res.status(401).json({message: "Invalid credentials"});
+      }
+      const token = jwt.sign({email, role: "admin"}, process.env.JWT_SECRET, {expiresIn: "1d"});
+      return res.status(200).json({ role: "admin", token });
     }
 
     const existUser = await User.findOne({ where: { email } });
---
+++ server/middlewares/onlyAdmin.middleware.js
@@ -0,0 +1,6 @@
+export default onlyAdmin = (req, res, next) => {
+    if(req.user?.role !== "admin") {
+        return res.status(403).json({message: "Your not admin"});
+    }
+    next();
+}
