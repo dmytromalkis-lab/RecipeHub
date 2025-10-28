eb3b4d80197cc741e0fcbf9b1d54dfe2a71c1a02
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2025-10-28 17:00:57 +0200

    Create recipe and get recipe by ID

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth.route.js";
 import userRouter from "./user.route.js";
+import recipeRouter from "./recipe.route.js";
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
+router.use("/recipes", recipeRouter);
 
 export default router;
