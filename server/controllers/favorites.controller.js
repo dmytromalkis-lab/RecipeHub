3c034451a48b915f986dc7f48888ae2cf93c447d
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2025-10-29 20:38:07 +0200

    FIx favorites endpoint

--- server/controllers/favorites.controller.js
+++ server/controllers/favorites.controller.js
@@ -1,18 +1,29 @@
 import { Op } from 'sequelize';
 import Favorites from "../models/favorites.model.js";
+import Recipe from '../models/recipe.model.js';
+import Category from '../models/category.model.js';
+import User from '../models/user.model.js';
+import Step from '../models/step.model.js';
+import Ingredient from '../models/ingredient.model.js';
 
 export const getFavorites = async (req, res) => {
   try {
-    if (!req.user || !req.user.id) {
-      throw new Error("User not authenticated");
-    }
-
     const userId = req.user.id;
 
     const favorites = await Favorites.findAll({
       where: {
         user_id: userId
-      }
+      },
+      include: [
+        { model: Recipe, 
+          attributes: ["recipe_id", "title", "description", "image_url", "difficulty", "prep_time", "serving", "updatedAt"],
+        include: [
+          { model: Step, attributes: ["description", "image_url", "step_number", "image_public_id"] },
+          { model: Ingredient, attributes: ["name", "quantity", "unit"] },
+          { model: Category, attributes: ["category_id", "category_name"] },
+          { model: User, as: "author", attributes: ["user_id", "first_name", "last_name", "avatar"] },
+        ]}
+      ]
     });
 
     return res.status(200).json({ favorites });
@@ -24,12 +35,14 @@ export const getFavorites = async (req, res) => {
 
 export const addFavorite = async (req, res) => {
   try {
-    if (!req.user || !req.user.id) {
-      throw new Error("User not authenticated");
-    }
-
     const userId = req.user.id;
     const recipeId = req.params.recipeId;
+
+    const existRecipe = await Recipe.findByPk(recipeId);
+
+    if(!existRecipe) {
+      return res.status(404).json({message: "Not found recipe. You cannot add recipe to favorites because recipe doesn`t found"})
+    }
 
     const favoriteCheck = await Favorites.findOne({
       where: {
@@ -49,18 +62,18 @@ export const addFavorite = async (req, res) => {
     return res.status(201).json({ message: "Favorite added", favorite });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error. Cannot get favorites", error });
+    res.status(500).json({ message: "Server error. Cannot add favorites", error });
   }
 };
 
 export const deleteFavorite = async (req, res) => {
   try {
-    if (!req.user || !req.user.id) {
-      throw new Error("User not authenticated");
-    }
-
     const userId = req.user.id;
     const recipeId = req.params.recipeId;
+
+    if(!existRecipe) {
+      return res.status(404).json({message: "Not found recipe. You cannot delete recipe to favorites because recipe doesn`t found"})
+    }
 
     const favoriteCheck = await Favorites.findOne({
       where: {
@@ -82,6 +95,6 @@ export const deleteFavorite = async (req, res) => {
     return res.status(200).json({ message: "Favorite deleted" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error. Cannot get favorites " + error });
+    res.status(500).json({ message: "Server error. Cannot delete favorites " + error });
   }
 };
