b09388c830307fdeb4020e5ced85ede60024cda2
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2025-11-26 21:28:46 +0200

    Create endpoint for get average rating for recipe

--- server/controllers/rating.controller.js
+++ server/controllers/rating.controller.js
@@ -1,4 +1,6 @@
 import Rating from "../models/rating.model.js";
+import Recipe from "../models/recipe.model.js";
+import { fn, col } from "@sequelize/core";
 
 export const createOrUpdateRating = async (req, res) => {
   try {
@@ -35,5 +37,43 @@ export const createOrUpdateRating = async (req, res) => {
     res
       .status(500)
       .json({ message: "Server error. Failed to create or update rating" });
+  }
+};
+
+export const getAverageRaiting = async (req, res) => {
+  try {
+    const { recipeId } = req.params;
+
+    const existingRecipe = await Recipe.findByPk(recipeId);
+    if (!existingRecipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    const result = await Rating.findOne({
+      where: { recipe_id: recipeId },
+      attributes: [
+        [fn("COUNT", col("rating")), "count"],
+        [fn("AVG", col("rating")), "average"],
+      ],
+      raw: true,
+    });
+
+    const count = parseInt(result.count);
+
+    if (!result || count === 0) {
+      return res.status(200).json({
+        message: "Recipe doesn't have ratings.",
+        averageRating: 0,
+        totalRatings: 0,
+      });
+    }
+
+    res.status(200).json({
+      averageRating: parseFloat(result.average),
+      totalRatings: count,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
   }
 };
--- server/routes/rating.route.js
+++ server/routes/rating.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createOrUpdateRating } from "../controllers/rating.controller.js";
+import {
+  createOrUpdateRating,
+  getAverageRaiting,
+} from "../controllers/rating.controller.js";
 import verifyToken from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/:recipeId", verifyToken, createOrUpdateRating);
+router.get("/:recipeId/average", getAverageRaiting);
+// router.get("/:recipeId/my", verifyToken, getMyRaitingForRecipe);
 
 export default router;
