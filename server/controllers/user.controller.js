aa4a4a5bbe7e6c9b7479eb51e7b6a93adcae3d07
NureKhudiienkoMykyta <mykyta.khudiienko@nure.ua>
2025-11-28 16:06:14 +0200

    Create endpoint for delete user by admin

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -99,3 +99,21 @@ export const updateAvatar = async (req, res) => {
     res.status(500).json({ message: "Server error. Error update avatar" });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const existUser = await User.findByPk(userId);
+
+    if (!existUser) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    await existUser.destroy();
+    res.status(200).json({ message: "User deleted" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error delete user!" });
+  }
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import verifyToken from "../middlewares/auth.middleware.js";
-import { getUserById, updateAvatar, updateUser } from "../controllers/user.controller.js";
+import {
+  getUserById,
+  updateAvatar,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.controller.js";
 import upload from "../middlewares/upload.middleware.js";
+import onlyAdmin from "../middlewares/onlyAdmin.middleware.js";
 
 const router = Router();
 
 router.get("/:id", getUserById);
 router.put("/:id", verifyToken, updateUser);
 router.put("/:id/avatar", verifyToken, upload.single("avatar"), updateAvatar);
+router.delete("/:id", verifyToken, onlyAdmin, deleteUser);
 
 export default router;
